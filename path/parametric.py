ebdc70b9bd5aa2366b92604446aa46d14b2c3a4d
Theo Lemay <theol0403@gmail.com>
2020-05-24 23:47:14 -0700

    path and parametric

---
+++ path/parametric.py
@@ -0,0 +1,29 @@
+from path import Path
+from point import Point
+from numpy import np
+
+
+# a function is a one-dimensional calculation that maps x to y
+class Function:
+    def calc(self, x):
+        pass
+
+    def calc_d(self, x):
+        pass
+
+
+# a parametric contains two functions that map t to x and y.
+# It therefore qualifies as a path.
+class Parametric(Path):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def calc(self, t):
+        x = self.x.calc(t)
+        y = self.y.calc(t)
+
+        x1 = self.x.calc_d(t)
+        y1 = self.y.calc_d(t)
+
+        return Point(x, y, np.arctan2(y1, x1))
---
+++ path/path.py
@@ -0,0 +1,8 @@
+
+class Path:
+
+    def calc(self, t):
+        pass
+
+    def interpolate(self, steps):
+        return [self.calc(i / steps) for i in range(steps)]
