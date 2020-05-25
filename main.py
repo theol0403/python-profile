d6db7d23cbfbd895d9e26fd29b64d79e253869a9
Theo Lemay <theol0403@gmail.com>
2020-05-25 14:12:00 -0700

    make paths

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ import matplotlib.pyplot as plt
 
 plt.title("Path")
 
-path = new_hermite(CubicHermite, Point(0, 0, 0), Point(1, 1, 0))
-path.plot("hermite")
+new_hermite(CubicHermite, Point(0, 0, 0), Point(1, 1, 0)).plot("Cubic Hermite")
+new_hermite(QuinticHermite, Point(0, 0, 0),
+            Point(1, 1, 0)).plot("Quintic Hermite")
 
-path = new_bezier([Point(0, 0), Point(1, 0),
-                   Point(0, 1), Point(1, 1)])
-path.plot("bezier")
+new_bezier([Point(1, 0), Point(1, 1), Point(0, 1)]).plot("Quadratic Bezier")
+
+new_bezier([Point(0, 0), Point(0, 1),
+            Point(1, 1), Point(1, 0)]).plot("Cubic Bezier")
+
+new_bezier([Point(0.0, 0.0),
+            Point(0.0, 1.0),
+            Point(0.5, 0.0),
+            Point(1.0, 1.0),
+            Point(1.0, 0.0)]).plot("Quartic Bezier")
+
+new_bezier([Point(1.0, 0.0),
+            Point(0.6, 0.0),
+            Point(1.0, 1.0),
+            Point(0.0, 0.6),
+            Point(0.0, 1.0)]).plot("Quintic Bezier")
+
+# new_bezier([Point(0, 0), Point(1, 0), Point(0, 1), Point(1, 1)]).plot("bezier")
 
 plt.legend()
 plt.show()
