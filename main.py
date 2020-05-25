9742799d91362dd599ce60e275c93db3ea8d761a
Theo Lemay <theol0403@gmail.com>
2020-05-25 11:20:20 -0700

    grid works

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ import matplotlib.pyplot as plt
 path = new_hermite(CubicHermite, Point(0, 0, 0), Point(1, 1, 0))
 
 points = [path.calc(i/100) for i in range(100)]
+x = [p.x for p in points]
+y = [p.y for p in points]
+
+plt.plot(x, y)
+plt.show()
