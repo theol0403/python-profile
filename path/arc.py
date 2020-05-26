e432ea850ed5bafcfb8856e45134854b034989e5
Theo Lemay <theol0403@gmail.com>
2020-05-26 13:09:53 -0700

    add arc

---
+++ path/arc.py
@@ -0,0 +1,43 @@
+import numpy as np
+from path import Path
+from point import Point
+
+
+class Arc(Path):
+    def __init__(self, start, end):
+        self.theta = end.theta - start.theta
+        c = start.dist(end)
+
+        if np.isclose(self.theta, 0.0):
+            self.r = np.Infinity
+            self.s = c
+        else:
+            self.r = c/(2.0*np.sin(self.theta/2.0))
+            self.s = self.r * self.theta
+
+        self.rotate = start.angle_to(end) - self.theta / 2.0
+        self.origin = start
+
+    def dist(self, t):
+        return self.s * t
+
+    def t_at_dist(self, d):
+        return d/self.s
+
+    def calc(self, t):
+        x, y = 0
+        if np.isinf(self.r):
+            y = self.s * t
+        else:
+            x = self.r * np.cos(t * self.theta) - self.r
+            y = self.r * np.sin(t * self.theta)
+
+        theta = self.rotate - np.PI/2.0
+
+        x_r = x * np.cos(theta) - y * np.sin(theta)
+        y_r = y * np.cos(theta) + x * np.sin(theta)
+
+        x_r += self.origin.x
+        y_r += self.origin.y
+
+        return Point(x_r, y_r, self.rotate + self.theta * t)
