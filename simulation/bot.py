e3ad9152ab5fe975f33bac709f441c9d66db6f76
mkkaufmann <mkaufmann421@gmail.com>
2020-05-27 01:00:38 -0400

    Cover edge cases in bot simulation

--- simulation/bot.py
+++ simulation/bot.py
@@ -1,5 +1,6 @@
 from path.point import Point
 import math
+import numpy as np
 
 class Bot:
     def __init__(self, wheel_track, orientation):
@@ -19,12 +20,26 @@ class Bot:
         return Bot(23, Point.origin())
 
     #http://www.cs.columbia.edu/~allen/F15/NOTES/icckinematics.pdf
+    #errors if wheeltrack == 0
     def move(self, left_vel, right_vel, dt):
-
         # signed distance from the instantaneous center of curvature (ICC)
-        r = (self.wheel_track/2)*((left_vel + right_vel)/(right_vel - left_vel))
+        r = 0
+
         # angular velocity
-        ang_vel = (right_vel - left_vel)/self.wheel_track
+        ang_vel = 0
+
+        #edge cases
+        if left_vel == right_vel:
+            r = np.Infinity
+        elif left_vel == -right_vel:
+            r = 0
+        elif left_vel == 0 or right_vel == 0:
+            r = self.wheel_track/2
+        else:
+            r = (self.wheel_track/2)*((left_vel + right_vel)/(right_vel - left_vel))
+            ang_vel = (right_vel - left_vel)/self.wheel_track
+
+
 
         icc = Point(self.orientation.x - r * math.sin(self.orientation.theta), self.orientation.y + r * math.cos(self.orientation.theta))
 
