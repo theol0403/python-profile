f1ed2da5a0c53b426344d47c2e81d04f939c9dcf
mkkaufmann <mkaufmann421@gmail.com>
2020-05-27 00:26:20 -0400

    Fix python errors (self in class methods)

--- simulation/bot.py
+++ simulation/bot.py
@@ -1,21 +1,22 @@
 from path.point import Point
+import math
 
 class Bot:
     def __init__(self, wheel_track, orientation):
         self.wheel_track = wheel_track
         self.orientation = orientation
 
-    def default15():
+    def default15(self):
         return Bot(14, Point.origin())
     
-    def default18():
+    def default18(self):
         return Bot(17, Point.origin())
 
-    def default24():
+    def default24(self):
         return Bot(23, Point.origin())
 
     #http://www.cs.columbia.edu/~allen/F15/NOTES/icckinematics.pdf
-    def move(left_vel, right_vel, dt):
+    def move(self, left_vel, right_vel, dt):
 
         # signed distance from the instantaneous center of curvature (ICC)
         r = (self.wheel_track/2)*((left_vel + right_vel)/(right_vel - left_vel))
