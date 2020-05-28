47afa8de7b53e6b2b0b6c5c084a39bd0e8fad223
Theo Lemay <theol0403@gmail.com>
2020-05-28 00:01:53 -0700

    fix trapezoid

--- generator/trapezoidal.py
+++ generator/trapezoidal.py
@@ -8,7 +8,7 @@ class Trapezoidal:
         self.length = length
 
         # the distance it takes to accelerate from 0 to full vel
-        a_dist = (vel**2)/2*accel
+        a_dist = (vel**2)/(2*accel)
         # the cruising at max vel distance
         c_dist = length - a_dist * 2
         # if cruise is negative, acceleration is too long and this is a triangular profile
@@ -20,18 +20,20 @@ class Trapezoidal:
         self.c_dist = c_dist
         self.top_vel = np.sqrt(2 * accel * a_dist)
 
-    def kinematics_at_d(self, d):
+    def calc_at_d(self, d):
         if d < 0 or d > self.length:
             print(f"Bad d: {d}")
 
         vel = 0
         if d <= self.a_dist:
             # we are accelerating
-            vel = np.sqrt(2 * accel * d)
+            vel = np.sqrt(2 * self.accel * d)
         elif d > self.a_dist and d < self.length - self.a_dist:
             # we are cruising
             vel = self.top_vel
         else:
             # we are decelerating
-            vel = np.sqrt(self.top_vel**2 - 2 * a *
-                          (d - self.length-self.a_dist))
+            vel = np.sqrt(self.top_vel**2 - 2 * self.accel *
+                          (d - self.a_dist - self.c_dist))
+
+        return vel
