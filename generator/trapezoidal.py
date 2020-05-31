41a64e426f0257741e897dbabf742de320a32922
Theo Lemay <theol0403@gmail.com>
2020-05-30 23:47:03 -0700

    add distance

--- generator/trapezoidal.py
+++ generator/trapezoidal.py
@@ -17,6 +17,9 @@ class Trapezoidal:
         self.top_vel = accel * self.t_accel
         self.time = self.t_accel * 2 + self.t_cruise
 
+        self.d_accel = 0.5 * accel * (self.t_accel ** 2)
+        self.d_cruise = self.top_vel * self.t_cruise
+
     def v_at_t(self, t):
         if t <= self.t_accel:
             return self.accel * t
@@ -24,3 +27,12 @@ class Trapezoidal:
             return self.top_vel
         else:
             return self.top_vel - (t - self.t_accel - self.t_cruise) * self.accel
+
+    def v_at_d(self, d):
+        if d <= self.d_accel:
+            return np.sqrt(2 * self.accel * d)
+        elif d > self.d_accel and d < self.length - self.d_accel:
+            return self.top_vel
+        else:
+            d_from_decel = d - self.d_accel - self.d_cruise
+            return np.sqrt(self.top_vel ** 2 - 2 * self.accel * d_from_decel)
