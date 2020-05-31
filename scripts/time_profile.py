915b56d585e6950a13b778b296bd6e4007b9f33e
Theo Lemay <theol0403@gmail.com>
2020-05-30 23:47:29 -0700

    clean

--- scripts/time_profile.py
+++ scripts/time_profile.py
@@ -2,14 +2,11 @@ from generator.trapezoidal import Trapezoidal
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 profile = Trapezoidal(1, 2, 10)
 dt = 0.01
 
 x = np.arange(0, profile.time + dt, dt)
 vels = [profile.v_at_t(t) for t in x]
-
-print(vels)
 
 plt.plot(x, vels)
 plt.title("Vel vs Time")
