476ab1a8a5051713b12c2d54e5b42387d1b3d877
Theo Lemay <theol0403@gmail.com>
2020-06-06 20:12:03 -0700

    time profile works fine

--- scripts/time_profile.py
+++ scripts/time_profile.py
@@ -1,9 +1,10 @@
-from generator.trapezoidal import Trapezoidal, TrapezoidalConstraints
+from generator.trapezoidal import Trapezoidal
+from generator.bot import Bot
 import matplotlib.pyplot as plt
 import numpy as np
 
-constraints = TrapezoidalConstraints(max_vel=2, max_accel=1)
-profile = Trapezoidal(constraints, 10)
+bot = Bot(track=1, max_vel=2, max_accel=1)
+profile = Trapezoidal(bot, 7)
 
 dt = 0.01
 x = np.arange(0, profile.time + dt, dt)
