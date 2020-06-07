89b87555110dfdebd08fa8cb9a8bd1e354e06e36
Theo Lemay <theol0403@gmail.com>
2020-06-06 21:22:50 -0700

    more logs

--- scripts/time_profile.py
+++ scripts/time_profile.py
@@ -5,10 +5,13 @@ import numpy as np
 
 bot = Bot(track=1, max_vel=2, max_accel=1)
 profile = Trapezoidal(bot, 3)
+print(vars(profile))
 
 dt = 0.01
 x = np.arange(0, profile.time + dt, dt)
 vels = [profile.v_at_t(t) for t in x]
+
+print(f"Distance: {np.sum(vels)*dt}")
 
 plt.plot(x, vels)
 plt.title("Vel vs Time")
