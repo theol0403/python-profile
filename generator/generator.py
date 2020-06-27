0fb486afbb8a53ed311a6e2032d629dba75ceb46
Theo Lemay <theol0403@gmail.com>
2020-06-26 23:37:46 -0700

    Revert "lachlan impl"
    This reverts commit c61ee6cbb726e24b4be0bc1ae2830e3d9b0f4ffb.

--- generator/generator.py
+++ generator/generator.py
@@ -16,28 +16,29 @@ def generate(*, bot, path, dt):
 
     trajectory = []
 
+    dist = profile.v_at_t(dt) * dt
     t = 0
-    vel = profile.v_at_t(dt)
-    pos = path.calc(t)
-    theta = pos.theta
-    curvature = path.curvature(t)
-    dist = 0
-
+    theta = path.calc(0).theta
     while dist <= length and t < 1.0:
-        d_dist = vel * dt
-        dist += d_dist
-        vel = profile.v_at_d(dist)
-
-        t = path.t_at_dist_travelled(t, d_dist)
-        pos_new = path.calc(t)
+        pos = path.calc(t)
         curvature = path.curvature(t)
 
+        vel = profile.v_at_d(dist)
+        vel = np.min([vel, bot.max_lin_vel_at_curvature(curvature)])
+
+        t_n = path.t_at_dist_travelled(t, vel * dt)
+
+        pos_new = path.calc(t_n)
         angular_vel = (pos_new.theta - theta) / dt
         theta += angular_vel * dt
+
         vel = np.min([vel, bot.max_lin_vel_at_angular_vel(angular_vel)])
 
+        d_dist = vel * dt
+        t = path.t_at_dist_travelled(t, d_dist)
+        dist += d_dist
+
         trajectory.append(Step(pos, vel, angular_vel, curvature))
-        pos = pos_new
 
     # find wheel speeds
     lin_vel = np.array([step.v for step in trajectory])
