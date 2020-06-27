26610f9016278f634fc3a08ecedc18887f92e99f
Theo Lemay <theol0403@gmail.com>
2020-06-26 22:24:23 -0700

    better length

--- path/__init__.py
+++ path/__init__.py
@@ -14,11 +14,10 @@ class Path:
         return [self.calc(i / steps) for i in range(steps + 1)]
 
     def length(self):
-        # s = 0
-        # for t in np.arange(0, 1, 0.01):
-        #     s += self.calc(t).dist(self.calc(t + 0.01))
-        # return s
-        return 0.001 * sum(map(lambda t: self.velocity(t), np.linspace(0, 1, 1001)))
+        s = 0
+        for t in np.arange(0, 1, 0.001):
+            s += self.calc(t).dist(self.calc(t + 0.001))
+        return s
 
     def t_at_dist_travelled(self, t, dist):
         return t + dist / np.abs(self.velocity(t))
